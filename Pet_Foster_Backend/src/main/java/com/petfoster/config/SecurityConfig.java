10b68139342b53ec8ed73f3854e4aa3b919c696e
swapsgit <swapnilsabswapnil@gmail.com>
2025-02-11 10:41:29 +0530

    working backend

---
+++ Pet_Foster_Backend/src/main/java/com/petfoster/config/SecurityConfig.java
@@ -0,0 +1,113 @@
+////package com.petfoster.config;
+////
+////import org.springframework.context.annotation.Bean;
+////import org.springframework.context.annotation.Configuration;
+////import org.springframework.security.authentication.AuthenticationManager;
+////import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+////import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+////import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+////import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+////import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+////import org.springframework.security.crypto.password.PasswordEncoder;
+////import org.springframework.security.web.SecurityFilterChain;
+////import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+////
+////import com.petfoster.utils.JWTUtils;
+////import com.petfoster.utils.JwtAuthenticationFilter;
+////
+////@Configuration
+////@EnableWebSecurity
+////public class SecurityConfig extends WebSecurityConfiguration {
+////
+////
+////	    private JWTUtils jwtUtils;
+////
+////	    public SecurityConfig(JWTUtils jwtUtils)
+////	    {
+////	    	this.jwtUtils = jwtUtils;
+////	    }
+////
+////    @Bean
+////    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+////	    {
+////	    	http.csrf(csrf -> csrf.disable())
+////	    		.authorizeHttpRequests(auth-> auth
+////	    		.requestMatchers("/login", "/signup").permitAll()
+////	    		.anyRequest().authenticated()
+////	    		)
+////	    		.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+////			
+////	    	return http.build();
+////	    }
+////	
+////	    @Bean
+////	    public PasswordEncoder passwordEncoder()
+////	    {
+////	    	return new BCryptPasswordEncoder();
+////	    }
+////	    
+////	    @Bean
+////	    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception
+////	    {
+////	    	return http.getSharedObject(AuthenticationManagerBuilder.class).build();
+////	    }
+////	    
+////	    @Bean
+////	    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+////	        return new JwtAuthenticationFilter(jwtUtils);
+////	    }
+////}
+
+package com.petfoster.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.petfoster.utils.JWTUtils;
+import com.petfoster.utils.JwtAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	private JWTUtils jwtUtils;
+
+	public SecurityConfig(JWTUtils jwtUtils) {
+		this.jwtUtils = jwtUtils;
+	}
+
+	// Define SecurityFilterChain bean instead of extending
+	// WebSecurityConfigurerAdapter
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth -> auth.requestMatchers("/users/login", "/users/signup", "*").permitAll() // Allow
+									.anyRequest().permitAll() // Require authentication for other requests
+				).addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(); // Password encoding bean
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+		return http.getSharedObject(AuthenticationManagerBuilder.class).build(); // Build authentication manager bean
+	}
+
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		return new JwtAuthenticationFilter(jwtUtils); // Create JWT filter bean
+	}
+}
---
+++ Pet_Foster_Backend/src/main/java/com/petfoster/controllers/FosterRequestController.java
@@ -0,0 +1,57 @@
+package com.petfoster.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.petfoster.modelDTO.FosterRequestDTO;
+import com.petfoster.services.FosterRequestService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/foster-request")
+public class FosterRequestController {
+
+	@Autowired
+	private FosterRequestService fosterRequestService;
+	
+	@PostMapping("/{user_id}/{pet_id}")
+	public ResponseEntity<FosterRequestDTO> addFosterRequest(@PathVariable Long user_id, @PathVariable Long pet_id, @RequestBody FosterRequestDTO fosterRequestDTO)
+	{
+		return ResponseEntity.ok(fosterRequestService.createFosterRequest(user_id, pet_id, fosterRequestDTO));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<FosterRequestDTO>> getAllFosterRequests()
+	{
+		return ResponseEntity.ok(fosterRequestService.getAllFosterRequests());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<FosterRequestDTO> getFosterRequestById(@PathVariable Long id)
+	{
+		return ResponseEntity.ok(fosterRequestService.getFosterRequestById(id));
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<FosterRequestDTO> updateFosterRequest(@PathVariable Long id, @RequestBody FosterRequestDTO fosterRequestDTO)
+	{
+		return ResponseEntity.ok(fosterRequestService.updateFosterRequest(id, fosterRequestDTO));
+	}
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteFosterReqeust(@PathVariable Long id)
+	{
+		fosterRequestService.deleteFosterRequest(id);
+		return ResponseEntity.noContent().build();
+	}
+}
