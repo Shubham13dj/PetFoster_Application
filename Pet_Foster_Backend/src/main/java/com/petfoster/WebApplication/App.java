32ef633a60f52267e87f8b5dac6ec4731c537502
swapsgit <swapnilsabswapnil@gmail.com>
2025-02-12 18:37:24 +0530

    PerformanceMonitoringAspect class added as a part of AOP
    com.petfoster.utils one new class to monitor the performance of the application

--- Pet_Foster_Backend/src/main/java/com/petfoster/WebApplication/App.java
+++ Pet_Foster_Backend/src/main/java/com/petfoster/WebApplication/App.java
@@ -3,6 +3,7 @@ package com.petfoster.WebApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(scanBasePackages= {"com.petfoster.services", "com.petfoster.config","com.petfoster.controllers","com.petfoster.utils"})
 @EntityScan(basePackages = {"com.petfoster.model", "com.petfoster.model.utility"})
 @EnableJpaRepositories(basePackages = "com.petfoster.repository")
+@EnableAspectJAutoProxy
 public class App 
 {
     public static void main( String[] args )
---
+++ Pet_Foster_Backend/src/main/java/com/petfoster/utils/PerformanceMonitoringAspect.java
@@ -0,0 +1,25 @@
+package com.petfoster.utils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class PerformanceMonitoringAspect {
+
+    @Pointcut("execution(* com.petfoster.services..*(..))")
+    public void monitor() {
+    }
+
+    @Around("monitor()")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+
+        System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        return proceed;
+    }
+}
